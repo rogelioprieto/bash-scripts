6eac933bce45d6eb758fe0cf1a15f51f2b512db3
rogelioprieto <rogelioprieto@gmail.com>
2022-02-07 21:12:10 -0700

    add two scripts

---
+++ README.md
@@ -0,0 +1,9 @@
+# bash-scripts repository
+ you can use it!
+
+Read the license:
+
+Creative Commons
+Attribution-ShareAlike 4.0 International
+<https://creativecommons.org/licenses/by-sa/4.0/>
+# bash-scripts
---
+++ generate-screenshots-from-pdf.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#author: Rogelio Prieto Alvarado
+#7/february/2021
+#Objective: generate screenshots from a pdf file in a reduce size (800px and png8)
+
+#Parametes: pdf filename
+
+#Requirements (packages):
+#1. poppler-utils (sudo apt-get install poppler-utils)
+#2. imagemagick (sudo apt install imagemagick)
+
+#Known errors:
+#1. Syntax Error (199275): Dictionary key must be a name object"
+#this erros appears to occurs for the pdf format file. You can ignore it!
+
+echo "ℹ️   Procesando el archivo: $1"
+#Step 1. Extract screenshots from pdf file.
+pdftocairo -png $1
+
+#Step 2. count the png screenshots files
+FILES=*.png
+OUTPUTFOLDER=screenshots
+#echo "FILES:"$FILES
+COUNTFILES=$(ls | wc -l)
+
+#Step 3. create a subfolder to move and save the screenshots
+if [ $COUNTFILES -eq 0 ]
+then
+    echo "No se generaron screenshots."
+else
+    echo "📄  Procesando $COUNTFILES screenshots..."
+    mkdir -p $OUTPUTFOLDER
+    #get filename without extension
+	filename=$(basename -- "$1")
+    filename="${filename%.*}"
+    mv $filename-*.png $OUTPUTFOLDER
+    cd $OUTPUTFOLDER
+    
+fi
+
+#Step 3. Move,resize and compress png screenshots
+WIDTH=800
+for file in $FILES
+do
+    convert $file -resize $WIDTHx\>  PNG8:$file
+    #example:
+    #convert image.png -resize 800x\>  PNG8:image.png
+done
+echo -e "👍 ¡listo!\n\nLas imágenes están almacenadas en el subdirectorio:\n📂 screenshots\n"
+
+
+#Sources:
+#https://graphicdesign.stackexchange.com/questions/120255/how-does-tinypng-com-minimize-pictures
+#https://legacy.imagemagick.org/Usage/resize/#resize
