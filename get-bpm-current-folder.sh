2c1bc5005a12b95fd64ad94e5d8275ff4f58e54c
rogelioprieto <rogelioprieto@gmail.com>
2022-03-21 17:53:17 -0700

    added minor changes

--- get-bpm-current-folder.sh
+++ get-bpm-current-folder.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#author: Rogelio Prieto Alvarado
+#22/march/2022
+#Objective: calculate the bpm for each mp3 in this directory or any subdirectories. Tag each file (mp3 tag).
+
 #initialize counter variable
 counter_file=0
 #process each .mp3 file
@@ -14,7 +18,6 @@ while read -r line ; do
     #echo "line=$line"
     bpm-tag $line
     #echo \"$line\" | echo
-
 
 done <<< "$(find . -name '*.mp3'   -type f  -follow -print)"
 #explanation about this subshell: https://stackoverflow.com/questions/16854280/a-variable-modified-inside-a-while-loop-is-not-remembered
