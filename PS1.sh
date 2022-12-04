0d3b002af6602331ddba1ea005805d2a10ea0b92
rogelioprieto <rogelioprieto@gmail.com>
2022-12-03 17:52:53 -0700

    added minor changes

--- PS1.sh
+++ PS1.sh
@@ -9,3 +9,6 @@ PS1="\e[0;32m[\u@\h] \$\e[m "
 
 #red color
 31
+
+#default mate colors
+PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]\[\033[01;34m\]:\[\033[00m\]\$ "
