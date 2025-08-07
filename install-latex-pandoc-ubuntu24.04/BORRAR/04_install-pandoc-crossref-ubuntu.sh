e83a60e81a430a23b0488bb783658f63817b1fc8
rogelioprieto <rogelioprieto@gmail.com>
2025-08-07 00:53:12 -0700

    added minor changes

---
+++ install-latex-pandoc-ubuntu24.04/BORRAR/04_install-pandoc-crossref-ubuntu.sh
@@ -0,0 +1,13 @@
+# author: Rogelio Prieto Alvarado
+# date: may 2021
+# note: execute this script using sudo.
+
+#install latest pandoc version
+sudo tar xvf pandoc-crossref-Linux-X64.tar.xz -C /usr/local/bin
+sudo mv /usr/local/bin/pandoc-crossref.1 /usr/local/man/man1
+echo -e  "\n\n---------------------------------------------------------------------"
+echo "The lastest pandoc-crossref version v0.3.20 (may 2025) was installed."
+echo "pandoc-crossref-Linux.tar.xz"
+echo "---------------------------------------------------------------------"
+echo "pandoc-crossref --version"
+pandoc-crossref --version
