8e880acf453e54de88a424bd87e4f94aea21834c
rogelioprieto <rogelioprieto@gmail.com>
2025-09-10 12:55:29 -0700

    added minor changes

--- compress-reduce-pdf-size.sh
+++ compress-reduce-pdf-size.sh
@@ -15,12 +15,44 @@ fi
 
 filename=$(basename -- "$1")
 extension="${filename##*.}"
-output="${filename%.*}-output.${extension}"
+#output="${filename%.*}-output.${extension}"
+output="${filename%.*}-output"
 
-#echo "filename:"${output}
+echo "Files will be created with these qualities:"
+echo "/screen    (72 DPI), smallest possible file size"
+echo "/ebook     (around 150 DPI) medium resolution"
+echo "/printer   (300 DPI)"
+echo "/prepress   highest quality setting"
+echo "/default   a balance between /screen and /printer"
+echo "----"
+#/screen    (72 DPI), smallest possible file size
+#/ebook     (around 150 DPI) medium resolution
+#/printer   (300 DPI)
+#/default   a balance between /screen and /printer
+#/prepress   highest quality setting 
 
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${output} $1
-echo "🟢  ${output} was built"
+
+
+
+
+
+for quality in screen ebook printer default prepress ;
+do
+    #echo ${quality}
+    outputfilename=${output}"-"${quality}"."${extension}
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/${quality} -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${outputfilename} $1
+    # Check the exit status
+    if [ $? -eq 0 ]; then
+        echo "🟢  ${outputfilename} was built"
+#        echo "Command executed successfully."
+#    else
+#      echo "Command failed."
+    fi   
+done
+
+
+
+
 
 #Source by: Adobe in
 #https://www.adobe.com/acrobat/hub/how-to/how-to-compress-pdf-in-linux
