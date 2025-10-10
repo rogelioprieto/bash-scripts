2e5dea241136723591a03ff10704938e2962eeff
rogelioprieto <rogelioprieto@gmail.com>
2025-10-10 13:16:58 -0700

    added minor changes

---
+++ 00-report-ml.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+01ZBB-generate_slides_to-markdown-v03.sh && 02Z-convert-slides-into-pptx-html-using-pandoc.sh && xdg-open slides.pptx
--- 02Z-convert-slides-into-pptx-html-using-pandoc.sh
+++ 02Z-convert-slides-into-pptx-html-using-pandoc.sh
@@ -12,16 +12,45 @@
 #pandoc slides.md --reference-doc=myref.pptx -o slides.pptx
 pandoc slides.md -o slides.pptx
 
+
+
+## Slidy (HTML)
+#TODO
+#pandoc -t slidy -s slides.md -o slides-slidy.html
+#pandoc -t slidy -s slides.md -o slides-slidy.html #pandoc uses the css and js from slidy website
+
+#pandoc -t slidy -s slides.md -o slides-slidy2.html \
+#  -V slidy-url=/home/$(whoami)/.pandoc/templates/slides/Slidy2 
+
+
+
+
 # DZSlides (HTML)
 #pandoc -t dzslides -s slides.md -o slides-dzslides.html
-
+#pandoc -t dzslides -s slides.md -o slides-dzslides.html \
+#  -V dzslides-url=/home/$(whoami)/.pandoc/templates/slides/dzslides/ 
 
 
 # With specific Reveal.js options
 # Requirement: wget https://github.com/hakimel/reveal.js/archive/master.tar.gz and uncompress as reveal.js subdirectory.
-pandoc -t revealjs -s slides.md -o slides-reveal.html \
-  -V theme=white -V transition=slide -V slideNumber=true \
-  -V revealjs-url=./reveal.js/
+# Reveal.js (HTML) - white theme
+#pandoc -t revealjs -s slides.md -o slides-revealjs-white.html \
+#  -V theme=white -V transition=slide -V slideNumber=true \
+#  -V revealjs-url=/home/$(whoami)/.pandoc/templates/slides/reveal.js/
+
+# Reveal.js (HTML) - sky theme
+#pandoc -t revealjs -s slides.md -o slides.html -V theme=sky
+#pandoc -t revealjs -s slides.md -o slides-revealjs-sky.html \
+#  -V theme=sky -V transition=slide -V slideNumber=true \
+#  -V revealjs-url=/home/$(whoami)/.pandoc/templates/slides/reveal.js/
+
 
 # s5 (HTML)
 #pandoc -t s5 -s slides.md -o slides-s5.html
+#pandoc -t s5 -s slides.md -o slides-s5.html \
+#  -V s5-url=/home/$(whoami)/.pandoc/templates/slides/s5-11/ui/default
+
+# slideous (HTML)
+#pandoc -t s5 -s slides.md -o slides-s5.html
+#pandoc -t slideous -s slides.md -o slides-slideous.html \
+#  -V slideous-url=/home/$(whoami)/.pandoc/templates/slides/slideous
