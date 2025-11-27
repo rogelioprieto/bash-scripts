ec35a4875fd22a42c85bea1ad53fee3da6e1dd56
rogelioprieto <rogelioprieto@gmail.com>
2025-11-27 15:16:25 -0700

    added minor changes

--- pandoc-update-date-in-default-yaml.sh
+++ pandoc-update-date-in-default-yaml.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-MYPATH=$HOME/.pandoc
+MYPATH=$HOME/.pandoc/metadata
+MYTEMPLATE=mytemplate.yaml
+MYMETADATAFILE=rpa.yaml
 
 #CURRENT_DATE=$(date +%Y-%m-%d)
 CURRENT_DATE=$(date +%B\ %d,\ %Y)
-sed "s/MYDATE_PLACEHOLDER/$CURRENT_DATE/" ${MYPATH}/mytemplate.yaml > ${MYPATH}/output.yaml
+sed "s/MYDATE_PLACEHOLDER/$CURRENT_DATE/" ${MYPATH}/${MYTEMPLATE} > ${MYPATH}/${MYMETADATAFILE}
+
+echo -e "date was updated in the yaml file:${MYPATH}/${MYMETADATAFILE}\n---"
+
