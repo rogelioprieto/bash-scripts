4328006524340a20034fac4b3b04e8b98f8fac07
rogelioprieto <rogelioprieto@gmail.com>
2026-02-13 10:23:32 -0700

    added minor changes

--- linuxtips-add-new.sh
+++ linuxtips-add-new.sh
@@ -2,72 +2,6 @@
 # date: 2026/01/14
 # author: Rogelio Prieto Alvarado
 
-
-
-
-
-###############
-# Custom Functions
-###############
-file_found() {
-#   local sum=$(($1 + $2)) # $1 and $2 refer to the first and second arguments
-#   echo "The sum of $1 and $2 is: $sum"
-    # echo "0"
-  # Functions use echo to return values which are then captured using command substitution
-
-    #0 file not found
-    #1 file was found
-
-
-    # Check if the file exists and is a regular file using the -f operator
-    if [[ -f "${1}" ]]; then
-        # echo "File '${1}' found."
-        # ls -l "${1}"
-        # head -10 "${1}"
-        # echo "(showing first 10 lines...)"
-        
-        # Ask for confirmation
-        read -p "File ${1} will be replaced with a NEW file. Do you want to proceed, ? (Y/n): " -n 1 -r REPLY
-        # echo # Move to a new line after user input
-
-        # Check the user's reply using a case-insensitive regular expression
-        if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-            # echo "Proceeding..."
-            echo "1"
-
-            # Add the commands you want to execute here
-        else
-            # echo "Aborting."
-            echo "0"
-            # exit 1 # Exit script with an error code
-        fi
-    else
-        echo "Error: File '${1}' not found."
-        echo "0"
-        # exit 1 # Exit script with an error code
-    fi
-
-}
-
-file_show_info() {
-        ls -l "${1}"
-        head -10 "${1}"
-        echo "(showing first 10 lines...)"
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Get the absolute path where script is stored.
 # SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}");
@@ -140,9 +74,6 @@ else
 fi
 
 
-
-
-
 #!/bin/bash
 
 # # Define the file path. Using double quotes prevents issues with spaces in names.
@@ -202,3 +133,54 @@ fi
     # fi
 
 # }
+
+
+# ###############
+# # Custom Functions
+# ###############
+# file_found() {
+# #   local sum=$(($1 + $2)) # $1 and $2 refer to the first and second arguments
+# #   echo "The sum of $1 and $2 is: $sum"
+#     # echo "0"
+#   # Functions use echo to return values which are then captured using command substitution
+
+#     #0 file not found
+#     #1 file was found
+
+
+#     # Check if the file exists and is a regular file using the -f operator
+#     if [[ -f "${1}" ]]; then
+#         # echo "File '${1}' found."
+#         # ls -l "${1}"
+#         # head -10 "${1}"
+#         # echo "(showing first 10 lines...)"
+        
+#         # Ask for confirmation
+#         read -p "File ${1} will be replaced with a NEW file. Do you want to proceed, ? (Y/n): " -n 1 -r REPLY
+#         # echo # Move to a new line after user input
+
+#         # Check the user's reply using a case-insensitive regular expression
+#         if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+#             # echo "Proceeding..."
+#             echo "1"
+
+#             # Add the commands you want to execute here
+#         else
+#             # echo "Aborting."
+#             echo "0"
+#             # exit 1 # Exit script with an error code
+#         fi
+#     else
+#         echo "Error: File '${1}' not found."
+#         echo "0"
+#         # exit 1 # Exit script with an error code
+#     fi
+
+# }
+
+# file_show_info() {
+#         ls -l "${1}"
+#         head -10 "${1}"
+#         echo "(showing first 10 lines...)"
+# }
+
