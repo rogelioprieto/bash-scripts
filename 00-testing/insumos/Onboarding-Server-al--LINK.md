a913903ca42360e90b46df974de717dd0a15eb15
rogelioprieto <rogelioprieto@gmail.com>
2026-05-14 23:56:02 -0700

    added minor changes

---
+++ 00-testing/insumos/Onboarding-Server-al--LINK.md
@@ -0,0 +1,1 @@
+<https://dev.to/oayanda/bash-scripting-how-to-onboard-multiple-users-on-ubuntu-linux-in-aws-ec2-4lae>
--- 00-testing/script-new-users.sh
+++ 00-testing/script-new-users.sh
@@ -37,5 +37,31 @@ done
 
 sudo newusers $myfilename
 
+# Missing Skeleton Files
+# and the users are assigned to the wrong shell (/bin/sh)
+# Solution:
+# copy the default configuration files from the /etc/skel directory
+# and assign the shell /bin/bash to users
+for ((i=1; i<=$user_number; i++)); do
+   this_user_prefix=$user_prefix$(printf "%02d" "$i")
+    sudo cp /etc/skel/.bashrc /etc/skel/.profile /home/$this_user_prefix/
+    sudo chown $this_user_prefix:$user_group /home/$this_user_prefix/.bashrc /home/$this_user_prefix/.profile
+    sudo usermod -s /bin/bash $this_user_prefix
+done
 
 
+# OPTION B
+# Linux systems include a built-in helper utility designed exactly for this scenario. Run this immediately after your newusers command:
+# sudo mkhomedir_helper username
+
+
+
+# for user in biomed2601 biomed2602 biomed2603 biomed2604 biomed2605 biomed2606 biomed2607 biomed2608 biomed2609 biomed2610 biomed2611; do                    
+#     sudo cp /etc/skel/.bashrc /etc/skel/.profile /home/$user/
+#     sudo chown $user:$user /home/$user/.bashrc /home/$user/.profile
+# done
+
+
+# for user in biomed2601 biomed2602 biomed2603 biomed2604 biomed2605 biomed2606 biomed2607 biomed2608 biomed2609 biomed2610 biomed2611 biomed2612; do
+#    sudo usermod -s /bin/bash $user
+# done
